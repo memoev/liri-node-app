d6c6ad6885c1342a5eb297b8d2421f14829010da
Guillermo Villalta <mexcelus@gmail.com>
2019-09-14 10:01:49 -0600

    swtich case

--- liri.js
+++ liri.js
@@ -3,3 +3,21 @@ const Spotify = require("node-spotify-api");
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var request = process.argv.slice(3).join('+');
+
+switch (command) {
+    case "concert-this":
+        console.log('concert');
+        break;
+    case "spotify-this-song":
+        console.log('spotify');
+        break;
+    case "movie-this":
+        console.log('movie');
+        break;
+    case "do-what-it-says":
+        console.log('just do it');
+        break;
+}
