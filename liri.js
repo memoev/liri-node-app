8f3f58422cbda46ca7adc5f700b1f70430091816
Guillermo Villalta <mexcelus@gmail.com>
2019-09-14 20:25:56 -0600

    removed comments

--- liri.js
+++ liri.js
@@ -17,8 +17,6 @@ function concertThis() {
         }
     }).catch(function (error) {
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
             console.log("---------------Data---------------");
             console.log(error.response.data);
             console.log("---------------Status---------------");
@@ -26,11 +24,8 @@ function concertThis() {
             console.log("---------------Status---------------");
             console.log(error.response.headers);
         } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
             console.log(error.request);
         } else {
-            // Something happened in setting up the request that triggered an Error
             console.log("Error", error.message);
         }
         console.log(error.config);
